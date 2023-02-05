651089ca416584172f05b1a6826acfe39a3be779
Misato42 <b9nom1k3@gmail.com>
2023-02-05 02:32:28 +0000

    入力を選択式に変更

--- src/main/java/com/tiscon/controller/EstimateController.java
+++ src/main/java/com/tiscon/controller/EstimateController.java
@@ -49,7 +49,11 @@ public class EstimateController {
     @GetMapping("input")
     String input(Model model) {
         if (!model.containsAttribute("userOrderForm")) {
-            model.addAttribute("userOrderForm", new UserOrderForm());
+            UserOrderForm form =  new UserOrderForm();
+            form.setBed("0");
+            form.setBicycle("0");
+            form.setWashingMachine("0");
+            model.addAttribute("userOrderForm", form);
         }
 
         model.addAttribute("prefectures", estimateDAO.getAllPrefectures());
